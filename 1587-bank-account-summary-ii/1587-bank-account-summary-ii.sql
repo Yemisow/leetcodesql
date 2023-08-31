6bf84ea8a09a10ca998f9a0601cf0a5e87f3bb14
Yemisow <110217288+Yemisow@users.noreply.github.com>
2023-08-31 17:57:52 +0100

    Time: 2808 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+
+select U.name,sum(T.amount) as balance
+from Users U
+left join 
+Transactions T
+on 
+U.account =T.account
+group by U.account
+having  balance > 10000

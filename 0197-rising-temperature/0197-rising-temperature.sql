cf7bddb6bdb788861641577a57a5d70dc97d9828
Yemisow <110217288+Yemisow@users.noreply.github.com>
2023-09-20 19:42:41 +0100

    Time: 891 ms (72.46%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,11 @@
 with cte as
-(select *,ADDDATE(recordDate,INTERVAL-1 DAY) as yesterday,LAG(temperature) Over (order by recordDate) as yesterdaytemp,lAG(recordDate) Over (order by recordDate) as previousday
-from Weather)
- 
- select id from cte
- where yesterday = previousday
- and temperature> yesterdaytemp
+(
+  select id,recordDate,temperature,subdate(recordDate,1) yesterday,lag(recordDate) over(order by recordDate) previousdate
+  ,lag(temperature) over (order by recordDate ) yesterdaytemp
+from Weather
+)
+
+
+select id
+from cte
+where temperature > yesterdaytemp and yesterday=previousdate

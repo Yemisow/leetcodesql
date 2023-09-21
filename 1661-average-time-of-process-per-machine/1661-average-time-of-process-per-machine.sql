cfc9ed4bea8af362e9094926250107ce6b5e2ec3
Yemisow <110217288+Yemisow@users.noreply.github.com>
2023-09-21 11:23:17 +0100

    Time: 446 ms (74.51%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,19 +1,21 @@
 # Write your MySQL query statement below
-
 with cte as
-(
-  select A.machine_id, A.process_id  , A.activity_type as act_A ,A.timestamp  as start_time, B.activity_type as act_B ,B.timestamp as end_time ,(B.timestamp-A.timestamp) as difference
-from Activity A
-inner join
-Activity B
-on
-A.machine_id =B.machine_id
-and
-A.process_id =B.process_id
-and
-A.activity_type= "start" and  B.activity_type= "end"
-)
+(select A.machine_id ,A.process_id ,A.activity_type startact ,B.activity_type endact,
+A.timestamp start_time , B.timestamp end_time
 
-select machine_id,round(avg(difference),3) as processing_time
+from Activity A
+join Activity B
+on 
+A.machine_id = B.machine_id
+ and A.process_id = B.process_id
+
+ and A.activity_type != B.activity_type
+ and
+A.timestamp < B.timestamp)
+
+
+
+select  machine_id,round(avg(end_time-start_time),3) processing_time
 from cte
-group by machine_id
+group by
+ machine_id

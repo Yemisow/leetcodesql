8344a95bf3699d62ecf25e84df2d535a0ac6edc3
Yemisow <110217288+Yemisow@users.noreply.github.com>
2023-09-24 19:17:05 +0100

    Time: 1282 ms (66.63%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,13 +1,11 @@
 # Write your MySQL query statement below
+
+
 with cte as
-(select delivery_id , customer_id ,order_date ,customer_pref_delivery_date,
-row_number() over (partition by customer_id order by customer_id,order_date) as rownum
-from Delivery
-)
-select round(count(customer_id) /(
- select count(customer_id) 
+(select *,(case when order_date=customer_pref_delivery_date then "immediate" else "scheduled" end) immshe ,
+row_number()over(partition by customer_id order by order_date) ordpos
+from Delivery)
+
+select round(sum(case when immshe = "immediate" then 1 else 0 end) * 100/count(immshe),2) immediate_percentage
 from cte
-where rownum=1 
-) *100 ,2) as immediate_percentage
-from cte
-where order_date = customer_pref_delivery_date and rownum=1
+where ordpos = 1

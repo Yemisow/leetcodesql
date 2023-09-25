790639a00527ee9ff76fbeb4109214bab9f79dd5
Yemisow <110217288+Yemisow@users.noreply.github.com>
2023-09-25 17:40:41 +0100

    Time: 1106 ms (44.63%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
 
-with cte as
-(select *,rank()over(partition by product_id order by change_date desc) as position
+with cte as 
+(select product_id,new_price,rank()over(partition by product_id order by change_date desc) ranpos
 from Products
-where change_date <="2019-08-16")
+where change_date <="2019-08-16" )
 
-select product_id,new_price as price 
+select product_id,new_price as price
 from cte
-where position =1
+where ranpos = 1
 union
-select product_id,10 as price
+select distinct product_id, 10 as price
 from Products
 where product_id not in (select product_id from cte)

a216ffdb2b2467e6b7b007a85c1b102dcc99ef5b
Yemisow <110217288+Yemisow@users.noreply.github.com>
2023-09-25 18:20:51 +0100

    Time: 1950 ms (61.65%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,12 @@
 # Write your MySQL query statement below
-
 with cte as
-(select *,sum(weight) over (order by turn) as Total_Weight
+(
+select person_name, sum(weight)over(order by turn asc) weig
 from Queue)
+
 
 select person_name
 from cte
-where Total_Weight <= 1000
-order by Total_Weight desc
-LIMIT 1
+where weig <=1000
+order by weig desc
+limit 1

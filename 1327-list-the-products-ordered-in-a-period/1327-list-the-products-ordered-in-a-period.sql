013488f4e547161d71b6285017dae8addbeb88b6
Yemisow <110217288+Yemisow@users.noreply.github.com>
2023-09-28 12:04:27 +0100

    Time: 1542 ms (71.60%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
+
 with cte as
-(select P.product_id ,P.product_name, P.product_category,O.order_date,O.unit,sum(O.unit) as unitsum
-
+(select P.product_id  , P.product_name,O.order_date   ,O.unit 
 from Products P
-join 
-Orders O
-on
-P.product_id = O.product_id
-where O.order_date between  "2020-02-01" and "2020-02-29"
-group by P.product_id ,P.product_name, P.product_category)
+left join Orders O
+on 
+p.product_id =O.product_id
+where date_format(order_date, "%Y-%m") = "2020-02")
 
-select product_name,unitsum as unit
+
+select product_name, sum(unit) as unit
 from cte
-where unitsum >= 100
+group by product_name
+having sum(unit) >= 100
